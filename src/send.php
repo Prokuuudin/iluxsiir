f39bf1b0888e10111af8100b30494c3cefb1c9a9
Prokuuudin <prokuuudin@gmail.com>
2025-01-27 10:37:55 +0200

    38

---
+++ src/send.php
@@ -0,0 +1,24 @@
+<?php
+echo '<pre>';
+var_dump($_POST);
+
+$url = 'https://google.com/recaptcha/api/siteverify';
+$data = ['secret' => '6LeJwZUqAAAAAMrbxL2n3XUKihLOCFePHqIpYQR9', 'response' => $_POST['g-recaptcha-response']];
+
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    ]
+];
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+$result = json_decode($result);
+var_dump($result);  // this will show the result of the reCAPTCHA verification
+
+if ($result->success) {
+    echo 'Success!';
+} else {
+    echo 'Failed!';
+}
